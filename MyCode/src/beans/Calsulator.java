b05646001f62c581a0681284c534af3da9f83271
user <user@192.168.1.162>
2018-11-16 10:02:01 +0530

    First commit
    uploading all files

---
+++ MyCode/src/beans/Calsulator.java
@@ -0,0 +1,15 @@
+package beans;
+
+public class Calsulator 
+{
+	public double add(double d1,double d2)
+	{
+		return d1+d2;
+	}
+	public double subtract(double d1,double d2)
+	{
+		return d1-d2;
+	}
+
+
+}
